df26a589ddd5c4ef3c08797adf300235160c0c9d
Kamran Akbar <kamranakbar502@gmail.com>
2023-06-26 13:34:50 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cut_my_garden
+
+Flutter Project from ITCoderz using Flutter SDK version 3.10.5 .
+
---
+++ android/app/src/main/java/com/itcoderz/cut_my_garden/MainActivity.java
@@ -0,0 +1,6 @@
+package com.itcoderz.cut_my_garden;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
